309caa483217ec98ae50711f5e9eb256aab4c7ba
Mina <minavu@Minas-MacBook-Pro.local>
2022-01-26 20:44:47 -0800

    Working on TrueDiamond jamTeam

--- TrueDiamond/src/main/java/edu/pdx/cs410J/jamTeam/Kata.java
+++ TrueDiamond/src/main/java/edu/pdx/cs410J/jamTeam/Kata.java
@@ -7,10 +7,35 @@ package edu.pdx.cs410J.jamTeam;
  * class (and its tests).
  */
 public class Kata {
+  public void printDiamond(char targetLetter) {
+    // https://stackoverflow.com/questions/10023818/shift-character-in-alphabet
+
+     char currentLetter = 'A';
+     int letterNumber = targetLetter - (int)'A';
+//     System.out.println(String.valueOf(letterNumber));
+     for (int i = letterNumber; i >= 0; --i) {
+         for (int j = 0; j < i; ++j) {
+             System.out.print(" ");
+         }
+         System.out.print(currentLetter);
+
+         if (currentLetter != 'A') {
+             int distance = 2 * (int) currentLetter + 1;
+
+             for (int k = 0; k <= distance; ++k) {
+
+             }
+         }
+
+     }
+  }
                                                                                     
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+//    System.err.println("Missing command line arguments");
+    Kata kata = new Kata();
+    kata.printDiamond('D');
     System.exit(1);
   }
+    // toEncode[i] = (toEncode[i] + 13 - (int)'a') % 26 + (int)'a';
 }
