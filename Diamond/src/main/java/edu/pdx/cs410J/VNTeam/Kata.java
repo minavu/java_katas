abaa956cf5bf71451f77b08f2661c9200ff4f246
ngqminh165 <minh25@pdx.edu>
2022-01-26 20:50:19 -0800

    finish

--- Diamond/src/main/java/edu/pdx/cs410J/VNTeam/Kata.java
+++ Diamond/src/main/java/edu/pdx/cs410J/VNTeam/Kata.java
@@ -10,6 +10,57 @@ public class Kata {
                                                                                     
 
   public static void main(String[] args) {
+    char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().toCharArray();
+
+    char input = 'F';
+
+    int index = 0;
+    for (char character: alphabet) {
+      index++;
+      if (character == input) break;
+    }
+    int num = 0;
+    int underIndex = index;
+
+    while(alphabet[num] <= input) {
+      for(int i=0; i < index;i++) {
+        System.out.print(" ");
+        if (i == index - 1) {
+          if (alphabet[num] == 'A')
+            System.out.println(alphabet[num]);
+          else {
+            System.out.print(alphabet[num]);
+            for (int j=0; j< (2*num-1);j++) {
+              System.out.print(" ");
+            }
+            System.out.println(alphabet[num]);
+          }
+        }
+      }
+      num++;
+      index--;
+    }
+    num = underIndex-2;
+    while(alphabet[num] >= 'A') {
+      for (int i=0; i < underIndex - num; i++) {
+        System.out.print(" ");
+      }
+      if (alphabet[num] != 'A') {
+        System.out.print(alphabet[num]);
+        for (int j=0; j < 2*num-1; j++ ){
+          System.out.print(" ");
+        }
+        System.out.println(alphabet[num]);
+      } else {
+        System.out.println(alphabet[num]);
+      }
+      num--;
+      if (num < 0)
+        break;
+    }
+
+    System.out.println("index: " + index);
+
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
